0d2b7296e3390fd988ab35ab154e7f3d74fa31f4
Baillivet Maxime <74535738+SluggerH@users.noreply.github.com>
2020-12-09 15:14:12 +0100

    Add files via upload

---
+++ testimonials.php
@@ -0,0 +1,14 @@
+<div class="testi container-fluid">
+
+    <div class="container">
+            <div class="testi_info">
+                <div class="testi_titre">
+                    <h2>Testimonials</h2>
+                    <img src="images/guillemets.png" alt="">
+                </div>
+                <p>Unerdwear cookie liquorice. Cake donut cupvake lollipop soufflé candy. Chocolate oat cake tootsie roll.</p>
+                <a href="">-Walter White</a>
+            </div>
+    </div>
+
+</div>
