efa4a498183fca6b6b24f1652451f3fdffd6c0ec
Baillivet Maxime <74535738+SluggerH@users.noreply.github.com>
2020-12-09 15:55:47 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,23 +1,32 @@
-<div class="container-footer">
-    <div class="menu-footer">
-        <div class="nav-footer">
-            <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Service</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Portfolio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-            </li>
-            </ul>
+<div class="footer container-fluid">
+
+    <div class="container footer">
+
+        <div class="menu-footer">
+
+            <div class="nav-footer">
+                <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Service</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                </ul>
+            </div>
+
         </div>
+        
+        <div class="copyright">
+            2020©
+        </div>
+        
     </div>
-    <div class="copyright">
-        2020©
-    </div>
+
 </div>
