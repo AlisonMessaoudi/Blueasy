f8f7219d450a8e1fbdfd516dc812b9debcce5de9
Baillivet Maxime <74535738+SluggerH@users.noreply.github.com>
2020-12-09 14:27:11 +0100

    Add files via upload

---
+++ twitter.php
@@ -0,0 +1,13 @@
+<div class="twitter container-fluid">
+
+    <div class="container">
+            <div class="twitter_info">
+                <div class="twitter_titre">
+                    <h2>Twitter</h2>
+                    <img src="images/twitter.png" alt="">
+                </div>
+                <p>Unerdwear cookie liquorice. Cake donut cupvake lollipop soufflé candy. Chocolate oat cake <br><a href="https://twitter.com/cheesecake?lang=en#:~:text=The%20Cheesecake%20Factory%20%F0%9F%8D%B0%20(%40Cheesecake)%20%7C%20Twitter">@Cheesecake</a> tootsie roll.</p>
+            </div>
+    </div>
+
+</div>
