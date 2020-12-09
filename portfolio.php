b8f2d35bccfa71aeeba6038fa674a61fb8485c8a
Alison Messaoudi <74536712+AlisonMessaoudi@users.noreply.github.com>
2020-12-09 13:03:30 +0100

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -1,1 +1,76 @@
-<p>je suis la partie portfolio</p>
+<section id="portfolio" class="container">
+
+    <div id="header_portfolio" class="container row">
+
+        <div class="column">
+            <h2>Portfolio</h2>
+            <div class="trait"></div>
+        </div>
+
+
+        <nav class="justify-content-end">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">All</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Panorama</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Portraits</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Macros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Journal</a>
+                </li>
+            </ul>
+        </nav>
+
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+    
+            <div class="col">
+                <img src="images/portfolio/1.png" alt="Image de pont"/>
+            </div>
+            <div class="col">
+                <img src="images/portfolio/2.png" alt="Image de pont brumeux"/>
+            </div>
+            <div class="col">
+                <img src="images/portfolio/3.png" alt="Image de lac brumeux"/>
+            </div>
+            <div class="col">
+                <img src="images/portfolio/4.png" alt="Image de lac brumeux"/>
+            </div>
+    
+        </div>
+
+        <div class="row">
+    
+            <div class="col">
+                <img src="IMAGES/PORTFOLIO/5.png" alt="Image de chalet en montagne"/>
+            </div>
+            <div class="col">
+                <img src="IMAGES/PORTFOLIO/6.png" alt="Image de vague avec un surfer"/>
+            </div>
+            <div class="col">
+                <img src="IMAGES/PORTFOLIO/7.png" alt="Image de Montagne"/>
+            </div>
+            <div class="col">
+                <img src="IMAGES/PORTFOLIO/8.png" alt="Image de Montagne"/>
+            </div>
+    
+        </div>
+    
+    </div>
+
+</div>
+
+
+
+</section>
+    
