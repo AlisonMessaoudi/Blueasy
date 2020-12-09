cc4d72a4a8fca51566173d47681046a891b58cfe
Baillivet Maxime <74535738+SluggerH@users.noreply.github.com>
2020-12-09 16:25:53 +0100

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -1,73 +1,74 @@
-<section id="portfolio" class="container">
+<section class="container-fluid">
 
-    <div id="header_portfolio" class="container row">
+    <div class="portfolio container">
 
-        <div class="column">
-            <h2>Portfolio</h2>
-            <div class="trait"></div>
+        <div class="header_portfolio">
+
+            <div class="portfolio-titre">
+                <h2>Portfolio</h2>
+                <div class="trait"></div>
+            </div>
+
+            <nav class="portfolio-menu">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link lien_menu active" aria-current="page" href=".photos">All</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="panoramas" href=".panoramas">Panoramas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="portraits" href=".portraits">Portraits</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="macros" href=".macros">Macros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="journal" href=".journal">Journal</a>
+                    </li>
+                </ul>
+            </nav>
+
         </div>
 
+        <div class="container mySlides">
 
-        <nav class="justify-content-end container_nav">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link lien_menu active" aria-current="page" href=".photos">All</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="panoramas" href=".panoramas">Panoramas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="portraits" href=".portraits">Portraits</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="macros" href=".macros">Macros</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="journal" href=".journal">Journal</a>
-                </li>
-            </ul>
-        </nav>
+            <div class="row ligne_slide">
+        
+                <div class="col slide">
+                    <img class="photos portraits" src="images/portfolio/1.png" alt="Image de pont"/>
+                </div>
+                <div class="col slide">
+                    <img class="photos panoramas" src="images/portfolio/2.png" alt="Image de pont brumeux"/>
+                </div>
+                <div class="col slide">
+                    <img class="photos journal" src="images/portfolio/3.png" alt="Image de lac brumeux"/>
+                </div>
+                <div class="col slide">
+                    <img class="photos macros" src="images/portfolio/4.png" alt="Image de lac brumeux"/>
+                </div>
+        
+            </div>
+
+            <div class="row ligne_slide">
+        
+                <div class="col slide">
+                    <img class="photos panoramas" src="IMAGES/PORTFOLIO/5.png" alt="Image de chalet en montagne"/>
+                </div>
+                <div class="col slide">
+                    <img class="photos portraits" src="IMAGES/PORTFOLIO/6.png" alt="Image de vague avec un surfer"/>
+                </div>
+                <div class="col slide">
+                    <img class="photos journal" src="IMAGES/PORTFOLIO/7.png" alt="Image de Montagne"/>
+                </div>
+                <div class="col slide">
+                    <img class="photos macros" src="IMAGES/PORTFOLIO/8.png" alt="Image de Montagne"/>
+                </div>
+        
+            </div>
+        
+        </div>
 
     </div>
-
-    <div class="container mySlides">
-
-        <div class="row ligne_slide">
-    
-            <div class="col slide">
-                <img class="photos portraits" src="images/portfolio/1.png" alt="Image de pont"/>
-            </div>
-            <div class="col slide">
-                <img class="photos panoramas" src="images/portfolio/2.png" alt="Image de pont brumeux"/>
-            </div>
-            <div class="col slide">
-                <img class="photos journal" src="images/portfolio/3.png" alt="Image de lac brumeux"/>
-            </div>
-            <div class="col slide">
-                <img class="photos macros" src="images/portfolio/4.png" alt="Image de lac brumeux"/>
-            </div>
-    
-        </div>
-
-        <div class="row ligne_slide">
-    
-            <div class="col slide">
-                <img class="photos panoramas" src="IMAGES/PORTFOLIO/5.png" alt="Image de chalet en montagne"/>
-            </div>
-            <div class="col slide">
-                <img class="photos portraits" src="IMAGES/PORTFOLIO/6.png" alt="Image de vague avec un surfer"/>
-            </div>
-            <div class="col slide">
-                <img class="photos journal" src="IMAGES/PORTFOLIO/7.png" alt="Image de Montagne"/>
-            </div>
-            <div class="col slide">
-                <img class="photos macros" src="IMAGES/PORTFOLIO/8.png" alt="Image de Montagne"/>
-            </div>
-    
-        </div>
-    
-    </div>
-
-</div>
 
 </section>
