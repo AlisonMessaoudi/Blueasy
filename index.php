517868bc0b2279c7fabf9eb643cf7f104c535d20
Baillivet Maxime <74535738+SluggerH@users.noreply.github.com>
2020-12-09 16:27:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,18 +8,15 @@
 
  
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
- <link rel="stylesheet" href="header.css">
- <link rel="stylesheet" href="services.css">
- <link rel="stylesheet" href="portfolio.css">
- <link rel="stylesheet" href="just.css">
- <link rel="stylesheet" href="twitter.css">
- <link rel="stylesheet" href="whois.css">
- <link rel="stylesheet" href="testimonials.css">
- <link rel="stylesheet" href="contact.css">
- <link rel="stylesheet" href="footer.css">
-
-
-
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="services.css">
+    <link rel="stylesheet" href="portfolio.css">
+    <link rel="stylesheet" href="just.css">
+    <link rel="stylesheet" href="twitter.css">
+    <link rel="stylesheet" href="whois.css">
+    <link rel="stylesheet" href="testimonials.css">
+    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="footer.css">
 
 
 </head>
@@ -37,25 +34,18 @@
   require('contact.php');
   require('footer.php');
 
-
 ?>
-
-
-    
-
-
-
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-     <!-- JQUERY -->
+
+    <!-- JQUERY -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
   <script src="header.js"></script>
   <script src="services.js"></script>
   <script src="porfolio.js"></script>
-
-
 
 </body>
 </html>
