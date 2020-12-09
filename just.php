41d4b3e46c07c8a9b862bd2861c2b38a62e130b6
Baillivet Maxime <74535738+SluggerH@users.noreply.github.com>
2020-12-09 14:01:31 +0100

    Add files via upload

---
+++ just.php
@@ -0,0 +1,17 @@
+<div class="just container-fluid">
+
+    <div class="container">
+        <div class="row">
+            <div class="just_titre col-6">
+                <h2>Just default section</h2>
+                <p>Bear claw marzipan bear claw applicake i love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps i love underwear.com. Soufflé cotton candy dessert candyice cream wafer gummies cheesecake brownie.</p>
+                <p>Bear claw marzipan bear claw applicake i love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps i love underwear.com. Soufflé cotton candy dessert candyice cream wafer gummies cheesecake brownie.</p>
+                <a href="#">Visit me</a>
+            </div>
+            <div class="just-video col-6">
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/zpOULjyy-n8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+
+</div>
