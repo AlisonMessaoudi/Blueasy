7286791ba7b1ed502cb8a2bcd33b83d72f5195f9
Joanna Hoffmann <74536698+Joananas13@users.noreply.github.com>
2020-12-09 13:33:19 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,28 @@
-<p>je suis le header</p>
+
+
+    <div class="container-fluid">
+
+      <div class="container-menu">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">BLU<span class="E">E</span>ASY</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Services</a>
+              <a class="nav-link" href="#">Features</a>
+              <a class="nav-link" href="#">Portfolio</a>
+              <a class="nav-link" href="#">Contact</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <div class="container-h1">
+        <p>"I am looking for the unexpected. </p>
+        <p>I am looking for things i have never seen before."</p>
+
+      </div>
+    </div>
