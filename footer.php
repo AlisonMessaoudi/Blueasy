f660cc918332664fb78367262fe1f1e9e4ead7b1
Joanna Hoffmann <74536698+Joananas13@users.noreply.github.com>
2020-12-09 14:40:39 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+<div class="container-footer">
+    <div class="menu-footer">
+        <div class="nav-footer">
+            <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Service</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            </ul>
+        </div>
+    </div>
+    <div class="copyright">
+        2020©
+    </div>
+</div>
