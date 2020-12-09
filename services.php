c0536e2fc6480aa98aed94aa9e6bdf4fdc0c7635
Baillivet Maxime <74535738+SluggerH@users.noreply.github.com>
2020-12-09 12:08:22 +0100

    Update services.php

--- services.php
+++ services.php
@@ -2,27 +2,27 @@
 
     <div class="container">
         <div class="service_titre">
-            <h1>Services</h1>
+            <h2>Services</h2>
         </div>
         <div class="service_text row">
             <div class="col-3">
             <img src="images/coffee.png" alt="">
-            <h2>Coffee</h2>
+            <h3>Coffee</h3>
             <p>Apple pie icing sweet. Brownie jelly-o applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
             </div>
             <div class="col-3">
             <img src="images/coffee.png" alt="">
-            <h2>Instant</h2>
+            <h3>Instant</h3>
             <p>Apple pie icing sweet. Brownie jelly-o applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
             </div>
             <div class="col-3">
             <img src="images/dslr.png" alt="">
-            <h2>Serious</h2>
+            <h3>Serious</h3>
             <p>Apple pie icing sweet. Brownie jelly-o applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
             </div>
             <div class="col-3">
             <img src="images/dslr.png" alt="">
-            <h2>Frame</h2>
+            <h3>Frame</h3>
             <p>Apple pie icing sweet. Brownie jelly-o applicake sweet roll liquorice bear claw. Jujubes carrot cake cotton candy sweet tart brownie. Tiramisu applicake jujubes.</p>
             </div>
         </div>
